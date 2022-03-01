7d29009a55c341c3521d7ffa6b5f2bb3a0ae7a06
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-02-28 23:31:02 -0300

    Add files via upload

---
+++ analise_rda.R
@@ -0,0 +1,20 @@
+
+# Análise Multivariada - Análise de Redundância (RDA) --------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 28/02/2022 -------------------------------------------------------------------------------------------------------------------------
+
+# Conceito da Análise de Redundância -------------------------------------------------------------------------------------------------------
+
+# Para realizar a análise de redundância é necessário duas matrizes x e y, sendo a matriz x composta
+# por variáveis explicativas (exemplo: variáveis ambientais), e a matriz y composta por variáveis
+# respostas (exemplo: abundância de espécies). Portanto, o objetivo dessa análise é encontrar componentes
+# de y que são combinações lineares de x que representam a variação de y.
+
+# A premissa dessa análise é que exista uma dependência linear da resposta das variáveis  em y
+# nas variáveis em x. Uma variação em x sempre dará uma variação em y.
+
+# "O RDA também busca encontrar o quanto da composição e abundância das espécies na comunidade estão
+# relacionadas com descritores ambientais, mas assume que existe uma resposta linear das espécies aos
+# gradientes ambientais." - Provetes, 2011
+
+
